986f734f21fb48bff87ebab24fdd530d3325a8b2
Rômulo Ferreira Fraga <romulo.fraga.dev@gmail.com>
2021-06-01 18:36:53 -0300

    Estilização e montagem  da Pagina Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="http://pm1.narvii.com/6616/4e1251dcbb7b1584a37891fe6f24db1ef418e085_00.jpg"
+            alt="John Doe"
+          />
+          <div>
+            <strong>Repository name</strong>
+            <p>Description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
